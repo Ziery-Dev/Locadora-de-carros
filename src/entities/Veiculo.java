775d191628bd1aa401051ad8fea1c94712db8211
Ziery-Dev <zieryDev@gmail.com>
2024-09-21 22:31:22 -0300

    Projeto Inciaido. Criado classe veiculo, fatura e aluguelCarro

---
+++ src/entities/Veiculo.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class Veiculo {
+	private String modelo;
+
+	
+	
+	//construtor
+	public Veiculo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+
+	//getters e setters
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+	
+	
+	
+	
+}
